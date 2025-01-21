aa39945c4c9ee8d3a226e412fde25864fd08a5d4
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-21 15:30:24 +0100

    add: input and first variables

---
+++ main.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('#bottone')
+const prezzo = 0.21
+
+btn.addEventListener('click', function() {
+    const nome = document.querySelector('#nome').value
+    const eta = document.querySelector('#fascia-eta').value
+    const km = parseInt(document.querySelector('#km').value)
+
+    console.log (nome, eta, km);
+    
+
+    
+})
