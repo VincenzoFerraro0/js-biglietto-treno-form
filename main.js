366ee946d3a16341120bcc54a8dcce5f5320d5f6
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-01-21 16:07:54 +0100

    add: js logic base

--- main.js
+++ main.js
@@ -2,12 +2,35 @@ const btn = document.querySelector('#bottone')
 const prezzo = 0.21
 
 btn.addEventListener('click', function() {
+
+    //Dichiaro le variabili selezionado i vari elementi in html
     const nome = document.querySelector('#nome').value
     const eta = document.querySelector('#fascia-eta').value
     const km = parseInt(document.querySelector('#km').value)
 
     console.log (nome, eta, km);
-    
 
-    
+    const nomePasseggero = document.querySelector('#nome-passeggero')
+    const offertaAplicata = document.querySelector('#offerta-applicata')
+    const carrozza = document.querySelector('#carrozza')
+    const codiceCp = document.querySelector('#codice-cp')
+    const costo = document.querySelector('#costo')
+
+    nomePasseggero.innerHTML = nome
+
+    //Elaboro il prezzo del biglietto a seconda della fascia d'età
+    if(eta == 'minorenne'){
+        offertaAplicata.innerHTML = 'Biglietto Ridotto'
+        costo.innerHTML = `${((prezzo * km) * 0.80).toFixed(2)} €`
+    }else if (eta == 'over65') {
+        offertaAplicata.innerHTML = 'Biglietto Ridotto Over 65'
+        costo.innerHTML = `${((prezzo * km) * 0.60).toFixed(2)} €`
+    }else {
+        offertaAplicata.innerHTML = 'Biglietto Standard'
+        costo.innerHTML = `${(prezzo * km).toFixed(2)} €`
+
+    }
+
+    carrozza.innerHTML = Math.floor(Math.random() * 10) + 1;
+    codiceCp.innerHTML = Math.floor(Math.random() * 99999) + 1;
 })
